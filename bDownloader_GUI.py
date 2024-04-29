78b8e553164ec8b0e9e209cd8d0d81dd8539ed60
Jonathan Barrett <13082494+Burritoless-Codec@users.noreply.github.com>
2024-04-28 23:36:43 -0600

    Started setting up GUI Version
    Gui Should be able to handle multiple urls at once W.I.P

---
+++ bDownloader_GUI.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+import subprocess
+import os
+
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.root.title('bDownloader GUI')
+        self.root.resizable(width=False, height=False)
+        self.root.geometry('600x290')
+
+        self.label_1 = tk.Label(self.root, text='Urls: Only 1 entry per line.', font=15)
+        self.label_1.pack(anchor=tk.W, padx=10)
+
+        self.url_text = tk.Text(self.root, height=5)
+        self.url_text.pack(padx=10)
+
+        self.label_2 = tk.Label(self.root, text='Filenames: Only 1 entry per line.', font=15)
+        self.label_2.pack(anchor=tk.W, padx=10)
+
+        self.filename_text = tk.Text(self.root, height=5)
+        self.filename_text.pack(padx=10)
+
+        self.button_frame = tk.Frame(self.root)
+        self.button_frame.columnconfigure(0, weight=1)
+        self.button_frame.columnconfigure(1, weight=1)
+
+        self.btn1 = tk.Button(self.button_frame, text='Download')
+        self.btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+        self.btn2 = tk.Button(self.button_frame, text='View in explorer', command=show_explorer)
+        self.btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+        self.button_frame.pack(padx=5, pady=5, fill='x')
+
+        self.btn3 = tk.Button(self.root, text='Github')
+        self.btn3.pack(padx=5, pady=5, fill='x')
+
+        self.root.mainloop()
+
+
+def show_explorer():
+    create_downloads_directory()
+    cwd = os.getcwd()
+    subprocess.Popen(f'explorer /enter,"{cwd}\\downloads"')
+
+
+def create_downloads_directory():
+    if not os.path.exists('downloads'):
+        os.mkdir('downloads')
+
+
+if __name__ == '__main__':
+    GUI()
